03d6624305a3d89b5954b712c6239454769a6fad
zisabella2026S <zisabella@vt.edu>
2026-09-18 11:03:49 -0400

    Added UserTest and ShopHandler Draft

---
+++ src/project1/ShopHandler.java
@@ -0,0 +1,13 @@
+package project1;
+public class ShopHandler
+{
+    //private ArrayList<Equipment> userEquipment;
+    //public int getValue() {
+        
+    //}
+    //public String getPerkDescription() {
+        
+    //}
+
+
+}
---
+++ src/project1/UserTest.java
@@ -0,0 +1,54 @@
+package project1;
+
+import student.TestCase;
+
+/**
+ * Tests the User class.
+ * 
+ * @author Isabella Zhang
+ * @version 2026.09.18
+ */
+public class UserTest extends TestCase
+{
+    private User user;
+
+    /**
+     * Sets up a User before each test.
+     */
+    public void setUp()
+    {
+        user = new User();
+    }
+
+
+    /**
+     * Tests setting and getting money.
+     */
+    public void testMoney()
+    {
+        user.setMoney(100);
+        assertEquals(100, user.getMoney());
+
+        user.setMoney(0);
+        assertEquals(0, user.getMoney());
+
+        user.setMoney(500);
+        assertEquals(500, user.getMoney());
+    }
+
+
+    /**
+     * Tests setting and getting the number of failed questions.
+     */
+    public void testNumFailedQuestions()
+    {
+        user.setNumFailedQuestions(3);
+        assertEquals(3, user.getNumFailedQuestions());
+
+        user.setNumFailedQuestions(0);
+        assertEquals(0, user.getNumFailedQuestions());
+
+        user.setNumFailedQuestions(10);
+        assertEquals(10, user.getNumFailedQuestions());
+    }
+}
