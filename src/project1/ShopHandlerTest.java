ddae64b8512a91ab3f9ee601e9b49c3b9e3386bb
zisabella2026S <zisabella@vt.edu>
2026-09-24 19:55:40 -0400

    update to ShopHandlerTest

--- src/project1/ShopHandlerTest.java
+++ src/project1/ShopHandlerTest.java
@@ -1,10 +1,65 @@
 package project1;
-public class ShopHandlerTest
+
+import student.TestCase;
+
+/**
+ * Tests ShopHandler.
+ */
+public class ShopHandlerTest extends TestCase
 {
-    //~ Fields ................................................................
+    private User user;
+    private ShopHandler shop;
+    private Equipment oven;
 
-    //~ Constructors ..........................................................
+    /**
+     * Sets up each test.
+     */
+    public void setUp()
+    {
+        user = new User();
+        user.setMoney(500);
 
-    //~Public  Methods ........................................................
+        shop = new ShopHandler(user);
 
+        oven = new Equipment(
+            200,
+            "Orders give more money");
+    }
+
+    /**
+     * Tests a successful purchase.
+     */
+    public void testPurchaseItem()
+    {
+        shop.purchaseItem(oven);
+
+        assertEquals(300, user.getMoney());
+        assertTrue(shop.hasEquipment(oven));
+        assertEquals(1, shop.getEquipmentCount());
+    }
+
+    /**
+     * Tests when the user cannot afford an item.
+     */
+    public void testPurchaseItemNotEnoughMoney()
+    {
+        user.setMoney(100);
+
+        shop.purchaseItem(oven);
+
+        assertEquals(100, user.getMoney());
+        assertFalse(shop.hasEquipment(oven));
+        assertEquals(0, shop.getEquipmentCount());
+    }
+
+    /**
+     * Tests purchasing null.
+     */
+    public void testPurchaseNull()
+    {
+        shop.purchaseItem(null);
+
+        assertEquals(500, user.getMoney());
+        assertEquals(0, shop.getEquipmentCount());
+    }
 }
