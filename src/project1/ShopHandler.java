46c82a2a85fdbcacc1c90c46d5e02e8c0303a5cc
zisabella2026S <zisabella@vt.edu>
2026-09-24 19:52:57 -0400

    Added ShopHandler

--- src/project1/ShopHandler.java
+++ src/project1/ShopHandler.java
@@ -1,87 +1,40 @@
 package project1;
+
+import java.util.ArrayList;
+
 public class ShopHandler
 {
-    package project1;
+    private ArrayList<Equipment> userEquipment;
+    private User user;
 
-    import java.util.ArrayList;
-
-    /**
-     * Handles purchasing equipment from the shop.
-     * Keeps track of the equipment owned by the user.
-     *
-     * @author Isabella Zhang
-     * @version 2026.09.24
-     */
-    public class ShopHandler
+    public ShopHandler(User user)
     {
-        private ArrayList<Equipment> userEquipment;
-        private User user;
+        this.user = user;
+        userEquipment = new ArrayList<Equipment>();
+    }
 
-
-        /**
-         * Creates a new ShopHandler for the given user.
-         *
-         * @param user
-         *     the user who is using the shop
-         */
-        public ShopHandler(User user)
+    public void purchaseItem(Equipment equipment)
+    {
+        if (equipment != null
+            && user.getMoney() >= equipment.getValue())
         {
-            this.user = user;
-            userEquipment = new ArrayList<Equipment>();
-        }
-
-
-        /**
-         * Purchases an equipment item if the user has enough money.
-         *
-         * @param equipment
-         *     the equipment the user wants to purchase
-         */
-        public void purchaseItem(Equipment equipment)
-        {
-            if (equipment != null
-                && user.getMoney() >= equipment.getValue())
-            {
-                user.setMoney(user.getMoney() - equipment.getValue());
-                userEquipment.add(equipment);
-            }
-        }
-
-
-        /**
-         * Returns whether the user owns a certain equipment item.
-         *
-         * @param equipment
-         *     the equipment to check
-         * @return true if the user owns the equipment
-         */
-        public boolean hasEquipment(Equipment equipment)
-        {
-            return userEquipment.contains(equipment);
-        }
-
-
-        /**
-         * Returns the number of equipment items the user owns.
-         *
-         * @return number of equipment items
-         */
-        public int getEquipmentCount()
-        {
-            return userEquipment.size();
-        }
-
-
-        /**
-         * Returns the user's equipment.
-         *
-         * @return list of equipment owned by the user
-         */
-        public ArrayList<Equipment> getUserEquipment()
-        {
-            return userEquipment;
+            user.setMoney(user.getMoney() - equipment.getValue());
+            userEquipment.add(equipment);
         }
     }
 
+    public boolean hasEquipment(Equipment equipment)
+    {
+        return userEquipment.contains(equipment);
+    }
 
+    public int getEquipmentCount()
+    {
+        return userEquipment.size();
+    }
+
+    public ArrayList<Equipment> getUserEquipment()
+    {
+        return userEquipment;
+    }
 }
